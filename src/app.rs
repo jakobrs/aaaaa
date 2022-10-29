88ef68ef660851cd316478ca7359c3b5ba31160e
jakobrs <jakobrs100@gmail.com>
2022-10-29 12:38:04 +0200

    unbreak

--- src/app.rs
+++ src/app.rs
@@ -4,8 +4,6 @@ use egui::{
 };
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
     m0: f64,
     v0: f64,
